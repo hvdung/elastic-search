4d09eb3691b47449c9ad069fbaf7ec872f26b1e8
hvdung14@gmail.com <hvdung14@gmail.com>
2018-09-17 13:52:38 +0700

    Elastic search using searchkick gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,34 +1,3 @@
-require 'elasticsearch/model'
 class Article < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-
-  def self.search(query)
-    __elasticsearch__.search(
-      {
-        query: {
-          multi_match: {
-            query: query,
-            fields: ['title^10', 'text']
-          }
-        },
-        highlight: {
-          pre_tags: ['<em>'],
-          post_tags: ['</em>'],
-          fields: {
-            title: {},
-            text: {}
-          }
-        }
-      }
-    )
-  end
-
-  settings do
-    mappings dynamic: false do
-      indexes :title, type: :text, analyzer: :english
-      indexes :text, type: :text, analyzer: :english
-    end
-  end
+  searchkick
 end
-Article.import(force: true)
